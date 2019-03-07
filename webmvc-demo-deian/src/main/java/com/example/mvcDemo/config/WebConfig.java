6d5c806754b3e854275a8a9f176f77f4706ae07d
Deian Mishev <deian.mishev@bulpros.com>
2019-03-07 10:00:16 +0200

    Project moved

--- webmvc-demo-deian/src/main/java/com/example/mvcDemo/config/WebConfig.java
+++ webmvc-demo-deian/src/main/java/com/example/mvcDemo/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.example.mvcDemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.resource.EncodedResourceResolver;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,14 +12,38 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import javax.annotation.PostConstruct;
 import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+//
+//    @Autowired
+//    private SpringTemplateEngine templateEngine;
+//
+//    @PostConstruct
+//    public void extension() {
+//        FileTemplateResolver resolver = new FileTemplateResolver();
+//        resolver.setSuffix(".html");
+//        resolver.setTemplateMode("HTML5");
+//        resolver.setPrefix("/templates/");
+//        resolver.setOrder(templateEngine.getTemplateResolvers().size());
+//        resolver.setCacheable(false);
+//        templateEngine.addTemplateResolver(resolver);
+//    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:tmp/");
+                .addResourceLocations("file:tmp/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new EncodedResourceResolver())
+                .addResolver(new PathResourceResolver());
     }
 
     @Bean("messageSource")
